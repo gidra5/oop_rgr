511a2025082035c319f318f5adb8e15a470c5150
gidra5 <hranitel.52@gmail.com>
2020-12-05 15:05:57 +0200

    sxas

---
+++ assets/cube_150.glslf
@@ -0,0 +1,7 @@
+#version 150 core
+
+out vec4 o_Color;
+
+void main() {
+    o_Color = vec4(0.5, 0.5, 0.5, 1.);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,143 @@
+extern crate piston_window;
+extern crate vecmath;
+extern crate camera_controllers;
+
+#[macro_use]
+extern crate gfx;
+extern crate shader_version;
+
+//----------------------------------------
+// Cube associated data
+
+gfx_vertex_struct!( Vertex {
+    a_pos: [i8; 4] = "a_pos",
+    // a_tex_coord: [i8; 2] = "a_tex_coord",
+});
+
+impl Vertex {
+    fn new(pos: [i8; 3]) -> Vertex {
+        Vertex {
+            a_pos: [pos[0], pos[1], pos[2], 1],
+        }
+    }
+}
+
+gfx_pipeline!( pipe {
+    vbuf: gfx::VertexBuffer<Vertex> = (),
+    u_model_view_proj: gfx::Global<[[f32; 4]; 4]> = "u_model_view_proj",
+    out_color: gfx::RenderTarget<::gfx::format::Srgba8> = "o_Color",
+    out_depth: gfx::DepthTarget<::gfx::format::DepthStencil> =
+        gfx::preset::depth::LESS_EQUAL_WRITE,
+});
+
+//----------------------------------------
+
 fn main() {
-    println!("Hello, world!");
+    use piston_window::*;
+    use gfx::traits::*;
+    use shader_version::Shaders;
+    use shader_version::glsl::GLSL;
+    use camera_controllers::{
+        FirstPersonSettings,
+        FirstPerson,
+        CameraPerspective,
+        model_view_projection
+    };
+
+    let opengl = OpenGL::V3_2;
+
+    let mut window: PistonWindow =
+        WindowSettings::new("piston: cube", [1280, 720])
+        .exit_on_esc(true)
+        .samples(4)
+        .graphics_api(opengl)
+        .build()
+        .unwrap();
+    window.set_capture_cursor(true);
+
+    let ref mut factory = window.factory.clone();
+
+    let vertex_data = vec![
+        Vertex::new([ 1,  1,  1]),
+        Vertex::new([ 1,  1, -1]),
+        Vertex::new([ 1, -1, -1]),
+        Vertex::new([ 1, -1,  1]),
+        Vertex::new([-1,  1,  1]),
+        Vertex::new([-1,  1, -1]),
+        Vertex::new([-1, -1, -1]),
+        Vertex::new([-1, -1,  1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0, 1, 2, 0, 2, 3, 
+        4, 5, 6, 4, 6, 7, 
+        0, 1, 4, 1, 4, 5, 
+        0, 3, 7, 0, 7, 4, 
+        2, 3, 7, 6, 7, 2, 
+        1, 2, 5, 5, 2, 6, 
+    ];
+
+    let (vbuf, slice) = factory.create_vertex_buffer_with_slice
+        (&vertex_data, index_data);
+
+    let glsl = opengl.to_glsl();
+    let pso = factory.create_pipeline_simple(
+            Shaders::new()
+                .set(GLSL::V1_50, include_str!("../assets/cube_150.glslv"))
+                .get(glsl).unwrap().as_bytes(),
+            Shaders::new()
+                .set(GLSL::V1_50, include_str!("../assets/cube_150.glslf"))
+                .get(glsl).unwrap().as_bytes(),
+            pipe::new()
+        ).unwrap();
+
+    let get_projection = |w: &PistonWindow| {
+        let draw_size = w.window.draw_size();
+        CameraPerspective {
+            fov: 60.0, near_clip: 0.1, far_clip: 1000.0,
+            aspect_ratio: (draw_size.width as f32) / (draw_size.height as f32)
+        }.projection()
+    };
+
+    let model = vecmath::mat4_id();
+    let mut projection = get_projection(&window);
+    let mut first_person = FirstPerson::new(
+        [0.5, 0.5, 4.0],
+        FirstPersonSettings{
+            mouse_sensitivity_horizontal: 0.25,
+            mouse_sensitivity_vertical: 0.25,
+            ..FirstPersonSettings::keyboard_wasd()
+        }
+    );
+
+    let mut data = pipe::Data {
+            vbuf: vbuf.clone(),
+            u_model_view_proj: [[0.0; 4]; 4],
+            out_color: window.output_color.clone(),
+            out_depth: window.output_stencil.clone(),
+        };
+
+    while let Some(e) = window.next() {
+        first_person.event(&e);
+
+        window.draw_3d(&e, |window| {
+            let args = e.render_args().unwrap();
+
+            window.encoder.clear(&window.output_color, [0.3, 0.3, 0.3, 1.0]);
+            window.encoder.clear_depth(&window.output_stencil, 1.0);
+
+            data.u_model_view_proj = model_view_projection(
+                model,
+                first_person.camera(args.ext_dt).orthogonal(),
+                projection
+            );
+            window.encoder.draw(&slice, &pso, &data);
+        });
+
+        if let Some(_) = e.resize_args() {
+            projection = get_projection(&window);
+            data.out_color = window.output_color.clone();
+            data.out_depth = window.output_stencil.clone();
+        }
+    }
 }
